444aef1567a287b9bd603da28392e46c03e16b16
= <jmvarberg@gmail.com>
2024-02-26 11:47:32 -0600

    added deployment to shinyapps.io

--- scripts/update_databases.R
+++ scripts/update_databases.R
@@ -4,6 +4,9 @@ library(dplyr)
 library(tidyr)
 library(data.table)
 library(curl)
+library(rsconnect)
 source("./scripts/NCBI_FTP_reference_table.R")
 source("./scripts/UniProt_FTP_reference_table.R")
 
+#push to shinyapps.io
+rsconnect::deployApp(appName = "protoDB_dev", appFiles = c("app.r", "./databases/ncbi_reference_table.fst", "./databases/uniprot_ids_reference_table.fst", "README.md"), account = "jmvarberg", server = "shinyapps.io") 
